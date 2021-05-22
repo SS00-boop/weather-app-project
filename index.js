a2403d54f5f70a98fe2a9fe6323924f1bba30d35
SS00-boop <83383586+SS00-boop@users.noreply.github.com>
2021-05-22 12:00:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index > 0 && < 6) {
+    if (index > 0 && index < 7) {
       forecastHTML =
         forecastHTML +
         ` 
