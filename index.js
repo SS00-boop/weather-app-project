b8ea8b307944dca47412f9759e05dfbd0482875a
SS00-boop <83383586+SS00-boop@users.noreply.github.com>
2021-05-22 12:12:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "b44a9bf56bc77c3722a82aafc305edd4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
