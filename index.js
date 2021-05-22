8d026e520e8dadced44ded9cb605072e1f9ddfd3
SS00-boop <83383586+SS00-boop@users.noreply.github.com>
2021-05-22 11:36:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+
+  return days[day];
 }
 
 function displayForecast(response) {
